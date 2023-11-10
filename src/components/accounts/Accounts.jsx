ad90612a7887893edce3bd8d9e9027d6b867c92e
RatishLife1012 <ratishg1012@gmail.com>
2023-11-10 14:44:44 +0530

    new

--- src/components/accounts/Accounts.jsx
+++ src/components/accounts/Accounts.jsx
@@ -1,13 +1,121 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Layout from '../../Pages/Layout'
+import "../../components/accounts/Accounts.css"; // Import the CSS file for styles
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import axios from 'axios';
 
 const Accounts = () => {
+  const [selectedOptions, setSelectedOptions] = useState({
+    column1: 'Option 1',
+    column2: 'Option o1',
+    column3: 'Option 1'
+  }); // State to track selected dropdown options for each column
+ 
+  const handleOptionChange = (event, column) => {
+    setSelectedOptions({
+      ...selectedOptions,
+      [column]: event.target.value
+    });
+  };
+
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [revenueOptions, setRevenueOptions] = useState([]);
+
+  const handleStartDateChange = (date) => {
+    setStartDate(date);
+  };
+
+  const handleEndDateChange = (date) => {
+    setEndDate(date);
+  };
+  
+  useEffect(() => {
+    // Make API call when the component mounts or when selectedOptions.column1 changes
+    
+      // Make an API call using Axios
+      axios.get('http://localhost:9090/v1/api/business/revenueTransaction/getAll')
+        .then(response => {
+          // Update revenueOptions state with the API response data
+          setRevenueOptions(response.data);
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching revenue options:', error);
+        });
+    }
+  , []); 
+
   return (
     <Layout>
-         <div>Accounts</div>
+    <div className="accounts-table-container" style={{ textAlign: 'center' }}>
+      <h2>Account</h2>
+      <table  className="w-full text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 ">
+      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 ">
+        <tr>
+          <th>Accounts:</th>
+          <th>Code:</th>
+          <th>D/C</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+          <select value={selectedOptions.column1} onChange={(e) => handleOptionChange(e, 'column1')}>
+                  <option value="Revenue">Revenue</option>
+                  {revenueOptions.map(option => (
+                    <option key={option.code} value={option.codeDescription}>
+                      {option.revenueTransactionId}
+                    </option>
+                  ))}
+                </select>
+          </td>
+          <td>
+            <select value={selectedOptions.column2} onChange={(e) => handleOptionChange(e, 'column2')}>
+              <option value="value1"> 1</option>
+              <option value="value2">Option 2</option>
+              <option value="value3">Option 3</option>
+            </select>
+          </td>
+          <td>
+            <select value={selectedOptions.column3} onChange={(e) => handleOptionChange(e, 'column3')}>
+              <option value="Option 1">Option 1</option>
+              <option value="Option 2">Option 2</option>
+              <option value="Option 3">Option 3</option>
+            </select>
+          </td>
+          <td>
+        <label>From:</label>
+        <DatePicker
+          selected={startDate}
+          onChange={handleStartDateChange}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+          dateFormat="MM/dd/yyyy"
+          placeholderText="Select start date"
+        />
+        <label>To:</label>
+        <DatePicker
+          selected={endDate}
+          onChange={handleEndDateChange}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+          dateFormat="MM/dd/yyyy"
+          placeholderText="Select end date"
+          />
+          </td>
+        </tr>
+        {/* Add more rows as needed */}
+      </tbody>
+    </table>
+    </div>
     </Layout>
-   
-  )
-}
+  );
+};
 
 export default Accounts
--- src/components/income_statement/Income_Statement.jsx
+++ src/components/income_statement/Income_Statement.jsx
@@ -4,8 +4,311 @@ import Layout from '../../Pages/Layout'
 export const Income_Statement = () => {
   return (
     <Layout>
-        <div>Income_Statement</div>
+        <div className='.centered-text-container 'style={{ textAlign: 'center',border : '1px solid black'}} >
+          <h3 style={{ border : '1px solid black'}}>ABC.Inc,</h3>
+          <h3 style={{ border : '1px solid black'}}>Income Statement</h3>
+          <table className='accounts-table' style={{ textAlign: 'center' }} >
+            <thead className="md:text-xs  text-xs  tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600 font-Inter">
+              <th className="px-7 py-3">Accural accounting</th>
+              <th className="px-7 py-3">Current period</th>
+              <th className="px-7 py-3">Prior period</th>
+              <th className="px-7 py-3">Connection from COA</th>
+            </thead>
+            <tbody className="px-4 py-3 text-sm border ">
+              <tr>
+                <td>Revenues</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Interest Income</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Other Income</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Finance Charge Income</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Sales Returns and Allowances</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Sales Discounts</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Total Revenue</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Cost of products</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr> 
+              <tr>
+                <td>Freight</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Inventory Adjustments</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Purchase Returns and Allowances</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                </tr>
+              <tr>
+                <td>Reserved</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Total COGS</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Advertising Expenses </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Auto Expense</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Bad Debt Expenses</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Bank charge </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Cash Over and Short </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Commission Expense </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Employee benefit Program</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Freight Expense</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Gifts Expense</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Insurance - general </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Professional  Fees </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>License Expense</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Maintenance Expense </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Meals and Entertainment </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Office Expense</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Payroll Taxes</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Printing </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Postage </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Lease rent </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Repairs Expense </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Salaries Expense </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>ReseSupplies Expense</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Utilities Expense </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Gain/loss on Sale of Assets </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Total Operating Expenses</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>EBITDA</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Amortization Expense</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Depreciation Expense</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>EBIT</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Interest Expense</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Profit before taxes</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Taxes–FIT Expense</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Net Profit after taxes NPAT</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Preferred dividends</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Net income attributable to common shareholder</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+
+
+
+            </tbody>
+          </table>
+        </div>
     </Layout>
-    
-  )
-}
+    )
+    }
