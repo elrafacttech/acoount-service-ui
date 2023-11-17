476e21b4717f12df72110dd9ee2e1d8a7f6db1bb
RatishLife1012 <ratishg1012@gmail.com>
2023-11-17 13:08:28 +0530

    changes

--- src/components/purchase/AddPurchase.jsx
+++ src/components/purchase/AddPurchase.jsx
@@ -8,10 +8,15 @@ import { useNavigate } from 'react-router-dom';
 const AddPurchase = () => {
 const [productName, setProductName] = useState('');
 const [productCategory, setProductCategory] = useState('');
-const [unitsPurchased, setUnitsPurchased] = useState('');
+const [units, setUnits] = useState('');
 const [unitCost, setUnitCost] = useState('');
+const [VatAppliable,setVatAppliable] = useState('');
 const [vat, setVat] = useState('');
 const [paymentReceipt, setPaymentReceipt] = useState('');
+const [inovice,setInovice] =useState('');
+const [customer,setCustomer] =useState('');
+const [otherTaxes,setOtherTaxes] = useState('');
+const [total,setTotal]=useState('');
 
 const navigate = useNavigate();
 
@@ -22,8 +27,9 @@ const handleFormSubmit = async (e) => {
     const response = await axios.post('https://your-backend-api.com/purchase', {
       productName,
       productCategory,
-      unitsPurchased,
+      units,
       unitCost,
+      VatAppliable,
       vat,
       paymentReceipt,
     });
@@ -42,32 +48,41 @@ return (
   <Layout>
     <form onSubmit={handleFormSubmit}>
       <label htmlFor="productName" className="lableStyle-name">Product Name:</label>
-      <input  class="form-group"  
+      <select  class="form-group"  
         type="text"
         id="productName"
         name="productName"
         value={productName}
         onChange={(e) => setProductName(e.target.value)}
-      />
+      >
+          <option value="">Select Product</option>
+          <option value="Product1">Product 1</option>
+          <option value="Product2">Product 2</option>
+
+      </select>
       <br />
       <br />
       <label htmlFor="productCategory" className="lableStyle-cat">Product Category:</label>
-      <input class="form-group" 
+      <select class="form-group" 
         type="text"
         id="productCategory"
         name="productCategory"
         value={productCategory}
         onChange={(e) => setProductCategory(e.target.value)}
-      />
+      >
+        <option value="">Select Product</option>
+          <option value="Product A">Product A</option>
+          <option value="Product B">Product B</option>
+      </select>
       <br />
       <br />
-      <label htmlFor="unitsPurchased" className="lableStyle-unitpurc">Units Purchased:</label>
+      <label htmlFor="units" className="lableStyle-units">Units:</label>
       <input class="form-group" 
         type="text"
-        id="unitsPurchased"
-        name="unitsPurchased"
-        value={unitsPurchased}
-        onChange={(e) => setUnitsPurchased(e.target.value)}
+        id="units"
+        name="units"
+        value={units}
+        onChange={(e) => setUnits(e.target.value)}
       />
       <br />
       <br />
@@ -81,6 +96,20 @@ return (
       />
       <br />
       <br />
+      <label htmlFor="VatAppliable" className="lableStyle-VatApplicable">VAT Applicable:</label>
+      <select class="form-group" 
+        type="text"
+        id="VatAppliable"
+        name="VatAppliable"
+        value={VatAppliable}
+        onChange={(e) => setVatAppliable(e.target.value)}
+      >
+       <option value="">Select Product</option>
+          <option value="yes">Yes</option>
+          <option value="no">No</option> 
+      </select>
+      <br />
+      <br />
       <label htmlFor="vat" className="lableStyle-vat">VAT:</label>
       <input class="form-group" 
         type="text"
@@ -91,16 +120,61 @@ return (
       />
       <br />
       <br />
-      <label htmlFor="paymentReceipt" className="lableStyle-cash">Cash/Credit:</label>
-      <input class="form-group" 
+      <label htmlFor="paymentReceipt" className="lableStyle-paymentReceipt">Payment Type:</label>
+      <select class="form-group" 
         type="text"
         id="paymentReceipt"
         name="paymentReceipt"
         value={paymentReceipt}
         onChange={(e) => setPaymentReceipt(e.target.value)}
+      >
+        <option value="">Select Product</option>
+          <option value="cash">Cash</option>
+          <option value="credit">Credit</option>
+      </select>
+      <br />
+      <br />
+      <label htmlFor="inovice" className="lableStyle-inovice">Inovice:</label>
+      <input class="form-group" 
+        type="text"
+        id="inovice"
+        name="inovice"
+        value={inovice}
+        onChange={(e) => setInovice(e.target.value)}
       />
       <br />
       <br />
+      <label htmlFor="customer" className="lableStyle-customer">Customer:</label>
+      <input class="form-group" 
+        type="text"
+        id="customer"
+        name="customer"
+        value={customer}
+        onChange={(e) => setCustomer(e.target.value)}
+      />
+      <br />
+      <br />
+      <label htmlFor="otherTaxes" className="lableStyle-otherTaxes">OtherTaxes:</label>
+      <input class="form-group" 
+        type="text"
+        id="otherTaxes"
+        name="otherTaxes"
+        value={otherTaxes}
+        onChange={(e) => setOtherTaxes(e.target.value)}
+      />
+      <br />
+      <br />
+      <label htmlFor="total" className="lableStyle-total">Total:</label>
+      <input class="form-group" 
+        type="text"
+        id="total"
+        name="total"
+        value={total}
+        onChange={(e) => setTotal(e.target.value)}
+      />
+      <br />
+      <br />
+
       <button  id="addbtn" type="submit" className="submit">Add</button>
     </form>
   </Layout>
--- src/components/sales/AddSales.jsx
+++ src/components/sales/AddSales.jsx
@@ -7,26 +7,49 @@ import { useNavigate } from 'react-router-dom';
 const AddSales = () => {
 const [productName, setProductName] = useState('');
 const [productCategory, setProductCategory] = useState('');
-const [unitsSold, setUnitsSold] = useState('');
+const [units, setUnits] = useState('');
 const [unitPrice, setUnitPrice] = useState('');
 const [invoiceValue, setInvoiceValue] = useState('');
+const [VatAppliable,setVatAppliable] = useState('');
 const [vat, setVat] = useState('');
 const [paymentReceipt, setPaymentReceipt] = useState('');
+const [invoice,setInvoice] = useState('');
+const [customer,setCustomer] = useState('');
+const [otherTaxes,setOtherTaxes] = useState('');
+const [total,setTotal] = useState('');
 
 const navigate = useNavigate();
 
+
+
 const handleFormSubmit = async (e) => {
   e.preventDefault();
+
+
+
+  useEffect(() => {
+    const calculatedInvoiceValue = unitsSold * unitPrice;
+    setInvoiceValue(calculatedInvoiceValue);
+  }, [unitsSold, unitPrice]);
+
+
 
   try {
     const response = await axios.post('https://your-backend-api.com/purchase', {
       productName,
       productCategory,
-      unitsSold,
+      units,
       unitPrice,
       invoiceValue,
       vat,
       paymentReceipt,
+      VatAppliable,
+      invoice,
+      customer,
+      otherTaxes,
+      total,
+
+
     });
 
     // Assuming your backend responds with data after successful submission
@@ -43,32 +66,43 @@ return (
   <Layout>
     <form onSubmit={handleFormSubmit}>
       <label htmlFor="productName" className="lableStyle-name">Product Name:</label>
-      <input  className="form-group"  
+      <select  className="form-group"  
         type="text"
         id="productName"
         name="productName"
         value={productName}
         onChange={(e) => setProductName(e.target.value)}
-      />
+        >
+           <option value="">Select Product</option>
+          <option value="Product1">Product 1</option>
+          <option value="Product2">Product 2</option>
+
+        </select>
+      
       <br />
       <br />
       <label htmlFor="productCategory" className="lableStyle-cat">Product Category:</label>
-      <input className="form-group" 
+      <select className="form-group" 
         type="text"
         id="productCategory"
         name="productCategory"
         value={productCategory}
         onChange={(e) => setProductCategory(e.target.value)}
-      />
+        >
+            <option value=""></option>
+          <option value="ProductA">Product A</option>
+          <option value="ProductA">Product B</option>
+
+      </select>
       <br />
       <br />
-      <label htmlFor="unitsSold" className="lableStyle-unitsold">Units Sold:</label>
+      <label htmlFor="units" className="lableStyle-units">Units:</label>
       <input className="form-group" 
         type="text"
-        id="unitsSold"
-        name="unitsSold"
-        value={unitsSold}
-        onChange={(e) => setUnitsSold(e.target.value)}
+        id="units"
+        name="units"
+        value={units}
+        onChange={(e) => setUnits(e.target.value)}
       />
       <br />
       <br />
@@ -82,16 +116,31 @@ return (
       />
       <br />
       <br />
-      <label htmlFor="invoiceValue" className="lableStyle">Invoice Value:</label>
+      <label htmlFor="invoiceValue" className="lableStyle-invoiceValue">Invoice Value:</label>
       <input className="form-group" 
         type="text"
         id="invoiceValue"
         name="invoiceValue"
         value={invoiceValue}
-        onChange={(e) => setInvoiceValue(e.target.value)}
+        readOnly
       />
       <br />
       <br />
+      <label htmlFor="VatAppliable" className="lableStyle-VatAppliable">VAT Appliable:</label>
+      <select className="form-group" 
+        type="text"
+        id="VatAppliable"
+        name="VatAppliable"
+        value={VatAppliable}
+        onChange={(e) => setVatAppliable(e.target.value)}
+      >
+          <option value=""></option>
+          <option value="Yes">Yes</option>
+          <option value="No">No</option>
+
+      </select>
+       <br/>
+      <br/>
       <label htmlFor="vat" className="lableStyle-vat">VAT:</label>
       <input className="form-group" 
         type="text"
@@ -102,16 +151,60 @@ return (
       />
       <br />
       <br />
-      <label htmlFor="paymentReceipt" className="lableStyle-cash">Cash/Credit:</label>
-      <input className="form-group" 
+      <label htmlFor="paymentReceipt" className="lableStyle-paymentReceipt">Payment Type:</label>
+      <select className="form-group" 
         type="text"
         id="paymentReceipt"
         name="paymentReceipt"
         value={paymentReceipt}
         onChange={(e) => setPaymentReceipt(e.target.value)}
+      >
+        <option value=""></option>
+          <option value="Cash">Cash</option>
+          <option value="Credit">Credit</option>
+      </select>
+      <br />
+      <br />
+      <label htmlFor="invoice" className="lableStyle-invoice">Invoice:</label>
+      <input className="form-group" 
+        type="text"
+        id="invoice"
+        name="invoice"
+        value={invoice}
+        onChange={(e) => setInvoice(e.target.value)}
       />
-      <br />
-      <br />
+      <br/>
+      <br/>
+      <label htmlFor="customer" className="lableStyle-customer">Customer:</label>
+      <input className="form-group" 
+        type="text"
+        id="customer"
+        name="customer"
+        value={customer}
+        onChange={(e) => setCustomer(e.target.value)}
+      />
+      <br/>
+      <br/>
+      <label htmlFor="otherTaxes" className="lableStyle-otherTaxes">otherTaxes:</label>
+      <input className="form-group" 
+        type="text"
+        id="otherTaxes"
+        name="otherTaxes"
+        value={otherTaxes}
+        onChange={(e) => setOtherTaxes(e.target.value)}
+      />
+      <br/>
+      <br/>
+      <label htmlFor="total" className="lableStyle-total">Total:</label>
+      <input className="form-group" 
+        type="text"
+        id="total"
+        name="total"
+        value={total}
+        onChange={(e) => setTotal(e.target.value)}
+      />
+      <br>
+      </br>
       <button id="addbtn" type="submit" className="submit">Add</button>
     </form>
   </Layout>
